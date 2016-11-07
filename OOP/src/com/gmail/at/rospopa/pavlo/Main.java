ed0777a2eed9fcb5a26d084e627368dc1b75726b
Pavlo Rospopa <rospopa.pavlo@gmail.com>
2016-11-07 19:26:48 +0200

    Another test commit.

--- OOP/src/com/gmail/at/rospopa/pavlo/Main.java
+++ OOP/src/com/gmail/at/rospopa/pavlo/Main.java
@@ -3,6 +3,7 @@ package com.gmail.at.rospopa.pavlo;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("123");
+        // write your code here
     }
 }
